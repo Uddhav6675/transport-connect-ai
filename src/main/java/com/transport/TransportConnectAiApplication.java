cb17e0a060d5434b467cb5dcbec12cd726a234c5
Uddhav675 <karaduddhav675@gmail.com>
2026-06-03 14:24:35 +0530

    feat(TCAI-1): initialize spring boot modular monolith project

---
+++ src/main/java/com/transport/TransportConnectAiApplication.java
@@ -0,0 +1,13 @@
+package com.transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportConnectAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportConnectAiApplication.class, args);
+	}
+
+}
